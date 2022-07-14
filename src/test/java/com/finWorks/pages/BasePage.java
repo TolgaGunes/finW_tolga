c8efbc314f1d69c05511e6e6101f0a3a793e2d70
walid <walidsher@hotmail.com>
2022-07-14 19:58:49 +0200

    contact step definition created

--- src/test/java/com/finWorks/pages/BasePage.java
+++ src/test/java/com/finWorks/pages/BasePage.java
@@ -1,4 +1,11 @@
 package com.finWorks.pages;
 
+import com.finWorks.utilties.Driver;
+import org.openqa.selenium.By;
+
 public class BasePage {
+    public void navigateTo(String module) {
+        String xpath = "//span[contains(text(),'"+module+"')]";
+        Driver.getDriver().findElement(By.xpath(xpath));
+    }
 }
